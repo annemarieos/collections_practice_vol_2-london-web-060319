4471c439e27d3bffb026514e79d98375f38e5bea
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-06-01 12:27:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,3 +49,13 @@ def begins_with_r(array)
    end 
    
    def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
